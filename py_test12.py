be55bf8d062508415543a693d3c797ec2e9b809c
hyunseok <prking94@naver.com>
2023-05-02 10:13:34 +0900

    python 생성자와 소멸자 및 class interface 변수 스터디 테스트 파일입니다.

--- py_test12.py
+++ py_test12.py
@@ -26,7 +26,7 @@ a = FourCal()
 # a.setdata(10,20)
 # print(a.first, a.second, type(a))
 # print(a.add())
-print('return된 EvenAdd의 값 = ' , a.EvenAdd())
+#print('return된 EvenAdd의 값 = ' , a.EvenAdd())
 
 ########################
 '''
@@ -35,23 +35,60 @@ python의 생성자와 소멸자
 ########################
 
 class ConCal:
+    #추가된 생성자 __init__ 메서드 생성자 
     def __init__(self, first, second) -> None:
          self.first = first
-         self.second = second         
-    
-         
-         
+         self.second = second    
+    def setdata(self, first, second):
+        self.first = first
+        self.second = second
+    def setmin(self) :
+        result = self.first - self.second
+        return result
+    def setmul(self) :
+        result = self.first * self.second
+        return result
+    def setdiv(self) :
+        result = self.first / self.second
+        return result
+
+######## 파이썬 클래스의 상속 ########
+# 기본 구조
+class MoreFourCal(ConCal):
+    def pow(self):
+        result = self.first ** self.second # 두 수의 거듭제곱
+        return result
+
+a = MoreFourCal(4,2)
+print(type(a))
+print(a.first)
+print(a.second)
+
+####### 클래스 변수 ########
+class Family:
+    lastname = 'kim'# 클래스 변수 선언
+    def plus(self, a, b):
+        print('plus 함수 값 : ', a+b)
+
+a = Family() # Family 인스턴스 생성
+b = Family() # Family 인스턴스 생성
+
+# Family.lastname = 'lee' # 클래스 변수 재할당.
+a.lastname = 'park' # 인스턴스 변수 할당
+b.lastname = 'hong' 
+
+a.plus(10, 20)
+
+print('Family.lastname = ', Family.lastname)
+print('a.lastname = ', a.lastname)
+print('b.lastname = ', b.lastname)
+
+### 클래스 변수와 인스턴스 변수
+# 클래스 변수 - 모든 인스턴스 사이에 공유된 값을 가진 변수
+print('Family Class 변수의 id값 =',  id(Family.lastname))
+
+# 인스턴스 변수 - 각각의 인스턴스 마다 독립한 변수
+print('a = Family()의 인스턴스 변수 a의 id 값 = ', id(a.lastname) )
+print('b = Family()의 인스턴스 변수 b의 id 값 = ', id(b.lastname) )
 
 
-
-
-         
-         
-         
-         
-         
-         
-         
-         
-
-            
